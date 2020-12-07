48463ffca8073c9c372a93668e2e1291a3ab249b
Anna Kołodziejczyk <anna.ak.kolodziejczyk@gmail.com>
2020-12-07 12:49:36 +0100

    Creation of board, text X and O

--- src/main/java/com/kodilla/tictactoe/TicTacToeApplication.java
+++ src/main/java/com/kodilla/tictactoe/TicTacToeApplication.java
@@ -1,25 +1,82 @@
 package com.kodilla.tictactoe;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.shape.Line;
 import javafx.stage.Stage;
+import javafx.util.Duration;
+
+import java.util.LinkedList;
+import java.util.List;
 
 public class TicTacToeApplication extends Application {
+    private Tile [][] board = new Tile[3][3];
+    private List<Tile> tiles = new LinkedList<>();
+    private boolean keepPlaying = true;
+    private boolean isComplete = true;
+    GridPane root = new GridPane();
+
     private Parent createContent() {
-        Pane root = new Pane();
-        root.setPrefSize(600,600);
-        for(int i = 0; i < 3; i++){
-            for(int j = 0; j < 3; j++){
+        GridPane root = new GridPane();
+        root.setPrefSize(600, 600);
+        root.setAlignment(Pos.CENTER);
+        root.setGridLinesVisible(true);
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
                 Tile tile = new Tile();
-                tile.setTranslateX(j * 200);
-                tile.setTranslateY(i * 200);
-                root.getChildren().add(tile);
+                root.add(tile, j, i);
+                tiles.add(tile);
+                board[j][i] = tile;
             }
         }
+        //horizontal
+        for (int y = 0; y < 3; y++){
+            tiles.add(new Tile(board[0][y], board[1][y], board[2][y]));
+        }
+        //vertical
+        for(int x = 0; x < 3; x++){
+            tiles.add(new Tile(board[x][0], board[x][1], board[x][2]));
+        }
+        //diagonals
+        tiles.add(new Tile(board[0][0], board[1][1], board[2][2]));
+        tiles.add(new Tile(board[2][0], board[1][1], board[0][2]));
+
         return root;
     }
+    public void verifyResult(){
+        for(Tile tile: tiles){
+            if(tile.isComplete()){
+                keepPlaying = false;
+                createAnimationLineForWinner(tile);
+                break;
+            }
+       }
+
+    }
+
+    public void createAnimationLineForWinner(Tile tile){
+    Line line = new Line();
+    line.setStartX(tile.getTiles()[0].getCenterX());
+    line.setStartY(tile.getTiles()[0].getCenterY());
+    line.setEndX(tile.getTiles()[0].getCenterX());
+    line.setEndY(tile.getTiles()[0].getCenterY());
+
+    root.getChildren().add(line);
+        Timeline timeline = new Timeline();
+        timeline.getKeyFrames().add(new KeyFrame(Duration.seconds(0.5),
+        new KeyValue(line.endXProperty(), board[2][2].getCenterX()),
+        new KeyValue(line.endYProperty(), board[2][2].getCenterY())));
+        timeline.play();
+    }
+
 
     public static void main(String[] args) {
         launch(args);
@@ -27,7 +84,8 @@ public class TicTacToeApplication extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-primaryStage.setScene(new Scene(createContent()));
-primaryStage.show();
+        primaryStage.setScene(new Scene(createContent()));
+        primaryStage.setTitle("TicTacToe");
+        primaryStage.show();
     }
 }
--- src/main/java/com/kodilla/tictactoe/Tile.java
+++ src/main/java/com/kodilla/tictactoe/Tile.java
@@ -4,15 +4,67 @@ import javafx.geometry.Pos;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 
 public class Tile extends StackPane {
-    public Tile(){
+    private static boolean isLastX = true;
+    Text text = new Text();
+    TicTacToeApplication application = new TicTacToeApplication();
+
+    private Tile[] tiles;
+
+    public Tile[] getTiles() {
+        return this.tiles;
+    }
+
+    public Tile(Tile... tiles) {
+        this.tiles = tiles;
         Rectangle border = new Rectangle(200, 200);
         border.setFill(null);
         border.setStroke(Color.BLACK);
+        Text text = new Text();
+        text.setFont(Font.font(100));
+        setOnMouseClicked(event -> {
+            System.out.println("Kliknieto");
 
-        setAlignment(Pos.BOTTOM_CENTER);
-      //  setMargin(border,0);
-        getChildren().addAll(border);
+            if (isLastX) {
+                text.setText("O");
+
+            } else {
+                text.setText("X");
+            }
+            isLastX = !isLastX;
+        });
+        setAlignment(Pos.CENTER);
+        getChildren().addAll(border, text);
+    }
+
+    public void drawX() {
+        text.setText("X");
+    }
+
+    public void drawO() {
+        text.setText("O");
+    }
+
+    public String getValue() {
+        return text.getText();
+    }
+
+    public double getCenterX() {
+        return getTranslateX() + 100;
+    }
+
+    public double getCenterY() {
+        return getTranslateY() + 100;
+    }
+
+    public boolean isComplete() {
+        if (tiles[0].getValue().isEmpty()) {
+            return false;
+        }
+        return tiles[0].getValue().equals(tiles[1].getValue()) &&
+                tiles[0].getValue().equals(tiles[2].getValue());
     }
 }
